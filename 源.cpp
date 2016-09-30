b1bc3aa4fa471430e3e5c7b88b794b84bf11943e
zhang-2qi <596353954@qq.com>
2016-09-30 09:22:42 +0800

    用了网上对卡麦克尔数的判定后竟然就成了...

--- 源.cpp
+++ 源.cpp
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 #include<cmath>
 #include<cstdio>
@@ -48,6 +49,18 @@ int main()
 	 int a[100];
 	 int opp[100];
 	 opp[0]=561;
+	 int prime[168]={2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107,
+109, 113, 127, 131, 137, 139, 149, 151 ,157, 163 ,167, 173 ,179, 181 ,191, 193, 197, 199, 211 ,223,
+227, 229, 233, 239, 241, 251, 257, 263, 269, 271 ,277 ,281 ,283, 293, 307, 311, 313, 317, 331 ,337,
+347, 349, 353, 359, 367, 373, 379, 383, 389, 397 ,401 ,409 ,419, 421, 431 ,433, 439, 443, 449 ,457,
+461, 463, 467, 479, 487, 491, 499, 503, 509, 521 ,523 ,541 ,547, 557, 563 ,569, 571, 577, 587 ,593,
+599, 601, 607, 613, 617, 619, 631, 641, 643, 647 ,653 ,659 ,661 ,673, 677 ,683 ,691, 701, 709 ,719,
+727, 733, 739, 743, 751, 757, 761, 769, 773, 787 ,797 ,809, 811 ,821, 823 ,827 ,829, 839, 853 ,857,
+859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937 ,941, 947 ,953, 967 ,971 ,977, 983 ,991 ,997};
+	 int isprime[1000];
+	 memset(isprime,0,1000);
+	for(int t=0;t<168;t++)
+		isprime[prime[t]]=1;
 	if(n<561)
 		printf("0\n");
 	else if(n<=600)
@@ -62,6 +75,12 @@ int main()
 		p[ti]=1;
 		int m1=fenjie(a,i,p);
 		if(m1==1)
+			continue;
+		int count1=0;
+		for(int ppl=0;ppl<m1;ppl++)
+			if(isprime[a[ppl]]==1)
+				count1++;
+		if(count1<=3)
 			continue;
 		 flag=0;
 		 flag2=1;
@@ -137,5 +156,113 @@ int main()
 		 for(int i=0;i<count;i++)
 		   printf("%d ",opp[i]);
 
+}
+}
+*/
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#pragma warning (disable:4996)
+using namespace std;
+
+bool fenjie(int i,int* m,int* length)
+{   
+	//int flag=1;
+	int n=0;
+	int q=2;
+	int pre=0;
+	while(i!=1)
+	{  
+		if(i%q==0)
+		{   //q++;
+			if(pre==q)
+				return 0;
+			pre=q;
+		    i=i/q;
+			m[n]=q;
+			n++;
+			q--;
+			
+		}
+		q++;
+	}
+	*length=n;
+	return 1;
+}
+
+int main()
+{  
+	int* a=new int[1000001];
+	int ii=0;
+	for(;ii<99997;ii++)
+	{  a[ii]=0;
+	 a[ii+1]=0;
+	 a[ii+2]=0;
+	 a[ii+3]=0;
+	}
+	for(;ii<100001;ii++)
+		a[ii]=0;
+	//a[2]=1;
+
+	for(int i=2;i<1000001;++i)
+	{  if(a[i]==0)
+	{
+		int j=2;
+		int b;
+		b=j*i;
+while(b<1000001)
+{  
+	a[b]=1;
+	++j;
+	b=j*i;
+	}
+	}
+	}
+//	printf("lalallaa");
+	int n;
+	scanf("%d",&n);
+	int p1[100],p2[100];
+	p2[0]=561;
+	int count=1;
+	if(n<561)
+		printf("0\n");
+	else if(n<601)
+		printf("1\n561\n");
+	else
+	{  int count1=0;
+	int length=0;
+		for(int i=1105;i<n;i++)
+		{  count1=0;
+		length=0;
+			if(i%2==0)
+		  continue;
+		bool m1=fenjie(i,p1,&length);
+		if(!m1)
+		continue;
+		if(length==1)
+			continue;
+		int flag=1;
+		for(int h=0;h<length;h++)
+		{   if(a[p1[h]]==0)
+		  { 
+		    if((i-1)%(p1[h]-1)==0)
+		      ;
+			else
+			{	flag=0;
+			    break;
+			}
+			count1++;
+		}
+		}
+			if(!flag||count1<3)
+				continue;
+			
+           p2[count]=i;
+		   count++;
+		}
+		printf("%d\n",count);
+		for(int i=0;i<count;i++)
+			printf("%d ",p2[i]);
+		printf("\n");
 }
 }
